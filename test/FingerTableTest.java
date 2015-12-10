8f2d258aadde7e40eb26696f6a52ca2256b1c7d6
thibault <thibault.blanc@yahoo.fr>
2015-12-10 14:48:21 +0100

    import

--- test/FingerTableTest.java
+++ test/FingerTableTest.java
@@ -1,16 +1,11 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import projet.ChordActor;
 import projet.FingerTable;
 import projet.Key;
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
 
 public class FingerTableTest {
 
